c8993723ea2a2e7b35e0e511b27b6ea4b4a5bfe0
Jeremiah <jclanc7507@gmail.com>
2022-01-16 20:40:44 -0500

    debugging generate functions and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 // Assignment code here
-
+var passwordText = document.querySelector("#password");
+// debugger;
+// Choose how long the password should be: 8-128 characters
 var length = Number(prompt("How many characters would you like your password to be?"));
-while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+    while (isNaN(length) || length < 8 || length > 128) 
+        length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
 
+// Choose what type of characters should be used
 var uppers = confirm("Would you like to use uppercase letters?");
 var lowers = confirm("Would you like to use lowercase letters?");
 var numbers = confirm("Would you like to use numbers?");
 var symbols = confirm("Would you like to use special characters?");
 
+// If incorrect inputs are used, prompt will loop until a correct responce is given
 while (!uppers && !lowers && !numbers && !symbols) {
   alert("You must select at least one character type!");
   uppers = confirm("Would you like to use uppercase letters?");
@@ -16,39 +21,27 @@ while (!uppers && !lowers && !numbers && !symbols) {
   symbols = confirm("Would you like to use special characters?");
 }
 
-window.addEventListener('load', function() {
-  generateNewPassword();
-});
+var rando
 
-function generateNewPassword() {
-  var password = "";
+var allowed = {}
+        if (uppers) password += rando(allowed.uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        if (lowers) password += rando(allowed.lowers = "abcdefghijklmnopqrstuvwxy");
+        if (numbers) password += rando(allowed.numbers = "0123456789");
+        if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+    var passwordLength = Math.floor(Math.random() * (128 - 8) + 8);
+    var password = "";
+// Potential types of characters to be used
+function writePassword() {
+    
+for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * allowed.length);
+   password += allowed.substring(randomNumber, randomNumber +1);
+};
 
-  var allowed = {};
-  if (uppers) password += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
-  if (lowers) password += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
-  if (numbers) password += rando(allowed.numbers = "1234567890");
-  if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
-
-  for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
-
-  document.getElementById("password").value = randoSequence(password).join("");
-}
-
-window.alert("Your password is: " + passwordText);
-console.log(passwordText);
-document.write(passwordText);
+document.getElementById("password").value = randoSequence(password).join("");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)};
